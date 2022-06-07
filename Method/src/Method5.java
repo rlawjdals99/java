8f774c121001c846a19b8357e10f054e93ac2c39
rlawjdals99 <106085194+rlawjdals99@users.noreply.github.com>
2022-06-07 18:15:38 +0900

    Add files via upload

---
+++ Method/src/Method5.java
@@ -0,0 +1,54 @@
+
+public class Method5 {
+
+	public static void main(String[] args) {
+
+
+	
+	
+	
+	
+	cdb c = new cdb();
+//    c.d1 = "홍길동";	
+//    c.data2();
+//	c.d3 = "이순신";
+//	c.data1();
+//    c.d3 = "유관순";
+//    System.out.println(c.data3());
+	
+    
+//    c.d2 = "강감찬";  private으로 선언한 객체 ㅣㅇ므로 main class에서 로드 못함
+    c.data2("강감찬");    
+    String result = c.data3();    
+    System.out.println(result);
+    
+    
+    
+    
+	}
+
+}
+class cdb{
+	String d1 = null; //void, 자료형 매소드 로드 가능
+	private String d2;
+	
+	public static String d3;  // 다 사용 가능
+	
+	public static void data1() {  // static 자료형 외에는 로드가 불가능
+//		System.out.println(d3);
+//		System.out.println(this.d2);   
+	}
+	
+	public void data2(String user) {
+       this.d2 = user;		
+		System.out.println(d2);
+		
+	}
+	
+	public String data3() {
+	String a= this.d2;
+		return a;	
+	}
+	
+	
+}

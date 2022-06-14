60d418ae4675e94c955b9816483e5d263f8c1185
rlawjdals99 <106085194+rlawjdals99@users.noreply.github.com>
2022-06-14 18:05:11 +0900

    Add files via upload

---
+++ src/time2.java
@@ -0,0 +1,40 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class time2 {
+
+
+	public static void main(String[] args) {
+
+		Timer timer = new Timer();  // Timer util을 이용하여 시간메소드를 사용합니다.
+//        timer.scheduleAtFixedRate(new fofo(), 1000, 5000);
+                         //(시작할 메소드&클래스, 몇초뒤에 시작, 반복시간)
+        	// scheduleAtFixedRate : 지속적으로 반복 작업을 할 경우
+	
+	
+	TimerTask tk = new TimerTask() {
+		int a = 0;
+		String adata[] = {"홍길동", "이순신", "강감찬", "유관순"};
+		@Override
+		public void run() {
+			         if (a>=4) {
+						a=0;
+					}
+         			System.out.println(adata[a]);
+                 a++;
+		}
+	};
+	timer.scheduleAtFixedRate(tk, 0, 5000);
+	
+	
+	
+	
+	
+	
+	}
+}
+class fofo extends TimerTask{  //TimerTask : 시간작업
+public void run() {  // Task에서 실행되는 메소드 입니다.
+	System.out.println("시간 함수 작동이 잘 될까요?");
+}
+}

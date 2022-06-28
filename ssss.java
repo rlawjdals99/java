a06812d06b7163206129895fea9696beb0c32429
rlawjdals99 <106085194+rlawjdals99@users.noreply.github.com>
2022-06-28 21:48:18 +0900

    Add files via upload

---
+++ ssss.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+public class ssss {
+
+	public static void main(String[] args) {
+
+		/* 숙제. ArrayList를 이용하여 결과값을 출력 되도록 합니다.
+		   원시데이터 => {"red", "green", "blue", "pink", "orange", "black", "white", "hotpink", "gray"}
+		   해당 데이터 색상 값중 6자 미만의 색상은 모두 삭제 후 결과 배열 값으로 생성하시오. 
+		   [orange, hotpink]	*/
+
+		String colors[] = {"red", "green", "blue", "pink", "orange", "black", "white", "hotpink", "gray"};
+		ArrayList<String> arc = new ArrayList<>();
+		int ea = colors.length;
+		int ee = colors[0].length();
+		int a;
+		for(a=0;a<ea;a++) {
+			if (colors[a].length()>5) {
+				arc.add(colors[a]);
+			}
+		}
+		System.out.println(arc);
+	}
+}
